d85c777abe85a14b652cd0e14d1b9fea2ecfbc34
miapferreira <mi.apferreira@gmail.com>
2021-01-23 16:01:32 -0300

    sensitive data-insert encrypt on s3 backend

--- ec2.tf
+++ ec2.tf
@@ -1,28 +1,27 @@
 resource "aws_instance" "WEB" {
-  ami                         = var.ami
-  count                       = 1
-  instance_type               = var.instance_type
-  key_name                    = "${var.key_pair}"
+  ami           = var.ami
+  count         = 1
+  instance_type = var.instance_type
+  key_name      = var.key_pair
   #availability_zone           = var.availability_zone
   #vpc_security_group          = "${aws_security_group.allow_ssh.id}" #Verificar como declara o security group
   associate_public_ip_address = true
 
-  provisioner "remote-exec" {
-    inline = [
-      "touch /tmp/test",
-    ]
-    connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      host        = "${self.public_ip}"
-      private_key = "${file("linux.pem")}"
-    }
-
-  }
+  #provisioner "remote-exec" {
+  #inline = [
+  #"touch /tmp/test",
+  #]
+  #connection {
+  #type        = "ssh"
+  #user        = "ec2-user"
+  #host        = "${self.public_ip}"
+  #private_key = "${file("linux.pem")}"
+  #}
+  #}
 
   root_block_device { # Definir um tamanho padrão de disco
-    volume_size = "${var.disk_size}"
-    volume_type = "${var.volume_type}"
+    volume_size = var.disk_size
+    volume_type = var.volume_type
   }
 
   tags = {
--- main.tf
+++ main.tf
@@ -5,9 +5,9 @@ provider "aws" {
 terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket = "testemichel8"
-    key    = "teste.tfstate"
-    region = "us-east-1"
-    encrypt = true      #inserindo criptografia no backend (s3)
+    bucket  = "testemichel8"
+    key     = "teste.tfstate"
+    region  = "us-east-1"
+    encrypt = true #inserindo criptografia no backend (s3)
   }
 }
