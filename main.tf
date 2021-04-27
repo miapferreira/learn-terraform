325e5bef65aab0ce323f320c3746756d27985c9a
miferreira8 <miaparecido@uolinc.com>
2021-04-26 21:27:51 -0300

    expressions-and-condictions

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = var.region  
+  region = var.region
 }
 
 terraform {
   backend "s3" {
     # Lembre de trocar o bucket para o seu, não pode ser o mesmo nome
-    bucket  = "bucket-de-estudos"
-    key     = "terraform.tfstate"
+    bucket  = "estudos-terraform"
+    key     = "michel.tfstate"
     region  = "us-east-1"
     encrypt = true #inserindo criptografia no backend (s3)
   }
